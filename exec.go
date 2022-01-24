6c9f9fac07373c94a30f699cbdc3effb396073b0
Gon <29622355+gonyyi@users.noreply.github.com>
2022-01-24 15:00:08 -0600

    Update exec.go

--- exec.go
+++ exec.go
@@ -1,16 +1,22 @@
-// (c) Gon Y. Yi 2021 <https://gonyyi.com/copyright>
-// Last Update: 12/07/2021
+// (c) Gon Y. Yi 2021-2022 <https://gonyyi.com/copyright>
+// Last Update: 01/13/2022
 
 package goexec
 
 import (
 	"bytes"
-	"github.com/gonyyi/gosl"
+	"errors"
+	"os"
 	"os/exec"
+	"syscall"
 )
 
 // Version is to identify this library.
-const Version gosl.Ver = "GoExec v0.1.0-1"
+const Version string = "GoExec v0.3.3"
+
+var (
+	ErrNoPID error = errors.New("no pid info")
+)
 
 // New creates Exec and return its pointer
 // Exec can be recycled thru `*Exec.Reset()`
@@ -23,23 +29,46 @@ func New(command string, args ...string) *Exec {
 
 // Exec runs command with optional arguments and piped standard input.
 type Exec struct {
-	BaseDir   string       // BaseDir where the code should be run
-	Command   string       // Command is the command or file to execute
-	Args      []string     // Args is command arguments
-	StdIn     bytes.Buffer // StdIn if any needs to be sent to
-	StdOut    bytes.Buffer // StdOut is a standard output from the run
-	StdErr    bytes.Buffer // StdErr is a standard error from the run
-	Async     bool         // Async will wait until the run finishes
+	id      int          // id is optional pool ID used by bucket
+	JobID   int          // JobID is optional ID for job
+	BaseDir string       // BaseDir where the code should be run
+	Command string       // Command is the command or file to execute
+	Args    []string     // Args is command arguments
+	StdIn   bytes.Buffer // StdIn if any needs to be sent to
+	StdOut  bytes.Buffer // StdOut is a standard output from the run
+	StdErr  bytes.Buffer // StdErr is a standard error from the run
+	Async   bool         // Async will wait until the run finishes
+
 	exitCode  int
 	pid       int   // pid is for the process ID
 	err       error // err if any
 	isRunning bool  // isRunning stores status
+
+	AtStart   func(*Exec)
+	AtFail    func(*Exec)
+	AtSuccess func(*Exec)
+}
+
+func (c *Exec) Set(baseDir, cmd string, args ...string) *Exec {
+	c.BaseDir = baseDir
+	c.Command = cmd
+	c.Args = args
+	return c
+}
+
+// ID will return Exec's ID if available
+func (c *Exec) ID() int {
+	return c.id
 }
 
 // Reset will reset execute
 func (c *Exec) Reset() {
-	c.BaseDir = ""
-	c.Command = ""
+	if c.BaseDir != "" {
+		c.BaseDir = ""
+	}
+	if c.Command != "" {
+		c.Command = ""
+	}
 	c.Args = c.Args[:0]
 	c.StdIn.Reset()
 	c.StdOut.Reset()
@@ -50,19 +79,25 @@ func (c *Exec) Reset() {
 	c.isRunning = false
 }
 
+// ExitCode returns exit code of failed job
+// This won't work for Windows
+func (c *Exec) ExitCode() int {
+	return c.exitCode
+}
+
 // PID returns job's process ID
 func (c *Exec) PID() int {
 	return c.pid
 }
 
-// Error returns an error that stored when the job finished or failed.
-// To find out if the job finished was successful, check this value.
+// Error returns an error that stored when the job jobDone or jobFailed.
+// To find out if the job jobDone was successful, check this value.
 func (c *Exec) Error() error {
 	return c.err
 }
 
 // IsRunning returns isRunning value.
-// When the Run starts, this will set to true, and when finished this will be false.
+// When the Run starts, this will set to true, and when jobDone this will be false.
 func (c *Exec) IsRunning() bool {
 	return c.isRunning
 }
@@ -76,9 +111,25 @@ func (c *Exec) Wait() {
 	}
 }
 
+// Kill will kill the currently jobRunning process
+func (c *Exec) Kill() error {
+	if c.PID() < 0 {
+		return ErrNoPID
+	}
+	proc, err := os.FindProcess(c.PID())
+	if err != nil {
+		return err
+	}
+	// Kill the process
+	return proc.Kill()
+}
+
 // Run will execute the command. Also, it takes a function `doneFn`,
-// and execute it when the job finished.
-func (c *Exec) Run(whenSucc, whenFail func(*Exec)) error {
+// and execute it when the job jobDone.
+// NOTE: the error this function returns is about when starting.
+//   any error while jobRunning will not be returned. It can be obtained
+//   from `*Exec.Error()`
+func (c *Exec) Run() error {
 	var err error
 
 	cmd := exec.Command(c.Command, c.Args...)
@@ -87,40 +138,47 @@ func (c *Exec) Run(whenSucc, whenFail func(*Exec)) error {
 	cmd.Stdout = &c.StdOut
 	cmd.Stderr = &c.StdErr
 
-	// If starting the command failed, it will end the func
+	// If starting the command jobFailed, it will end the func
 	if err = cmd.Start(); err != nil {
 		c.pid = -1
 		c.err = err
+		// FAILED
+		if c.AtFail != nil {
+			c.AtFail(c)
+		}
 		return err
 	}
 
-	// Since there isn't an error starting the command,
-	// set the isRunning = true,
-	// set the pid
 	c.isRunning = true
 	c.pid = cmd.Process.Pid
 
-	do := func() {
-		if err = cmd.Wait(); err != nil {
-			c.err = err
-			if whenFail != nil {
-				whenFail(c)
-			}
-		} else {
-			if whenSucc != nil {
-				whenSucc(c)
-			}
-		}
-		c.isRunning = false
+	if c.AtStart != nil {
+		c.AtStart(c)
 	}
-	// If Async == true, wait until the process is finished
-	// Otherwise run it in background
+
 	if c.Async { // Async == true, wait until the job finishes
-		do()
-	} else { // Async == false, running as a background process
+		c.run(cmd)
+	} else { // Async == false, jobRunning as a background process
 		go func() {
-			do()
+			c.run(cmd)
 		}()
 	}
 	return c.Error()
+}
+
+func (c *Exec) run(cmd *exec.Cmd) {
+	if err := cmd.Wait(); err != nil {
+		if msg, ok := err.(*exec.ExitError); ok { // there is error code
+			c.exitCode = msg.Sys().(syscall.WaitStatus).ExitStatus()
+		}
+		c.err = err
+		if c.AtFail != nil {
+			c.AtFail(c)
+		}
+	} else {
+		if c.AtSuccess != nil {
+			c.AtSuccess(c)
+		}
+	}
+	c.isRunning = false
 }
